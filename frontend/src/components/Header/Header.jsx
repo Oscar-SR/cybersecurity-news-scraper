985d79868d47384c112ad687d3b7d9c8009d7bef
Óscar Sánchez Rubio <oscarsanchezrubio.03@gmail.com>
2025-12-26 13:44:00 +0100

    feat(frontend): add padding to header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import ThemeToggle from "../ThemeToggle/ThemeToggle";
-import styles from "./Header.module.css"; // importamos el módulo CSS
+import styles from "./Header.module.css"; 
 
 function Header({ theme, setTheme }) {
   return (
     <header className={styles.appHeader}>
-      <h1 className={styles.pageTitle}>Cybersecurity News Scraper</h1>
-      <ThemeToggle theme={theme} setTheme={setTheme} />
+      <div className="container d-flex justify-content-between align-items-center">
+        <h1 className={styles.pageTitle}>Cybersecurity News Scraper</h1>
+        <ThemeToggle theme={theme} setTheme={setTheme} />
+      </div>
     </header>
   );
 }
