ec025a7ee3abb78833c26bfe48639d184ce4d502
Óscar Sánchez Rubio <oscarsanchezrubio.03@gmail.com>
2026-06-10 11:05:21 +0200

    refactor: unify details in package.json

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const PORT = parseInt(process.env.BACKEND_PORT || "3000", 10);
+const PORT = Number.parseInt(process.env.BACKEND_PORT || "3000", 10);
 
 app.listen(PORT, () => {
     // Usamos i18next para traducir el mensaje
